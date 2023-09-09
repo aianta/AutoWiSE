48838333a67dc3337bc54c134f050c91bb46a5cd
Alex Ianta <aianta@dal.ca>
2023-09-09 11:07:16 -0600

    skeleton modal for creating recruitment campaigns

--- src/main/groovy/ca/ualberta/autowise/SlackBolt.groovy
+++ src/main/groovy/ca/ualberta/autowise/SlackBolt.groovy
@@ -2,13 +2,16 @@ package ca.ualberta.autowise
 
 import com.slack.api.bolt.App
 import com.slack.api.bolt.AppConfig
+import com.slack.api.bolt.handler.builtin.GlobalShortcutHandler
 import com.slack.api.bolt.jetty.SlackAppServer
+import com.slack.api.bolt.response.Response
 import com.slack.api.bolt.socket_mode.SocketModeApp
 import com.slack.api.socket_mode.SocketModeClient
 import groovy.transform.Field
 import io.vertx.core.json.JsonObject
 import org.slf4j.LoggerFactory
-import ca.ualberta.autowise.AutoWiSE
+
+import com.slack.api.model.view.View;
 
 import static ca.ualberta.autowise.scripts.slack.BlockingSlackMessage.*
 
@@ -35,11 +38,95 @@ class SlackBolt implements Runnable{
 
         this.app = new App(boltConfig)
 
-        this.app.command("/do_external_tick", (req, ctx) -> {
-            log.info "neat!"
-            autoWiSE.doExternalTick(services, config)
+        this.app.viewSubmission("create_new_campaign", (req, ctx)->{
+
+            def stateValues = req.getPayload().getView().getState().getValues()
+            String eventName = stateValues.get("event_name_block").get("event_name").getValue()
+
+            log.info "Event name was: ${eventName}"
+
             return ctx.ack()
         })
+
+        this.app.command("/new_vol_recruit_campaign", (req, ctx) -> {
+            log.info "neat!"
+
+            def response = ctx.client().viewsOpen{
+                it.triggerId(req.getPayload().getTriggerId())
+                        .viewAsString("""
+{
+	"type": "modal",
+	"callback_id": "create_new_campaign",
+	"title": {
+		"type": "plain_text",
+		"text": "My App",
+		"emoji": true
+	},
+	"submit": {
+		"type": "plain_text",
+		"text": "Submit",
+		"emoji": true
+	},
+	"close": {
+		"type": "plain_text",
+		"text": "Cancel",
+		"emoji": true
+	},
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Let's create a new event"
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "input",
+			"block_id": "event_name_block",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "event_name"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Event Name",
+				"emoji": false
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Create ",
+						"emoji": true
+					},
+					"value": "create_event",
+					"action_id": "0"
+				}
+			]
+		}
+	]
+}    """)
+            }
+
+            if(response.isOk()){
+                return ctx.ack()
+            }else{
+                return Response.builder().statusCode(500).body(response.getError()).build()
+            }
+
+        })
+
+
 
         socketModeApp = new SocketModeApp(
                 config.getString("socket_mode_token"),
